4b193b99894945aae4c96de08798c967483dcad6
Matthew Davidson <sunyata@gmail.com>
2011-08-03 23:37:59 +0200

    Initial import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject CrossWords "0.0.2-SNAPSHOT"
+  :description "A wonderful birthday program to turn sentences into crossword puzzles"
+  :dependencies [[clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.clojars.automata/rosado.processing "1.1.0"]]
+  :main CrossWords.core)
